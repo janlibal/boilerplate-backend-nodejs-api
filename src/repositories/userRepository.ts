0cd0279a02b62785441ca02d207935010df8ff12
Jan Libal <libalj@seznam.cz>
2023-12-28 10:20:11 +0100

    saveUser method implementation

--- src/repositories/userRepository.ts
+++ src/repositories/userRepository.ts
@@ -1,13 +1,19 @@
 import  { User }  from "../database/models"
+import { IUser } from "../interfaces/IUser"
 
 async function findByEmail(email: string) {
     let user: any
     user = await User.query().where('email', email).first()
     return user
+} 
+
+async function saveUser(attributes:IUser) {
+    let user: any
+    user = await User.query().insertAndFetch(attributes)
+    return user
 }
 
-
-
 export default {
-    findByEmail
+    findByEmail,
+    saveUser
 }
