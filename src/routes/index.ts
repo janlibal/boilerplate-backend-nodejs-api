dfef41d7e4b01b58f441256d26913f3a39e462fe
Jan Libal <libalj@seznam.cz>
2023-12-28 11:10:57 +0100

    create user route implementation

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -3,10 +3,13 @@ const router = new Router()
 const api = new Router()
 
 import testRoutes from './testRoutes'
+import userRoutes from './userRoutes'
+
 
 api.use(testRoutes)
+api.use(userRoutes)
 
 router.use('/api/v1', api.routes())
- 
+  
 
 export default router
