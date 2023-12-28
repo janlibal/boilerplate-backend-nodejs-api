5f63a8fc323d00e410d3dd99633fb89f8fa7ddba
Jan Libal <libalj@seznam.cz>
2023-12-28 10:37:17 +0100

    generateAccessToken method implementation

--- src/utils/crypto.ts
+++ src/utils/crypto.ts
@@ -1,10 +1,23 @@
 import crypto from 'crypto'
 import bcrypt from 'bcryptjs'
+import jwt, { SignOptions, VerifyOptions } from 'jsonwebtoken'
 import config from '../config'
+
+
+const signOptions: SignOptions = {
+    algorithm: config.auth.createOptions.algorithm, 
+    expiresIn: config.auth.createOptions.expiresIn,
+    issuer: `${config.auth.createOptions.issuer}.${config.server.environment}`
+}
 
 
 function hashPassword(password: string) {
     return bcrypt.hash(peperify(password), config.auth.saltRounds)
+}
+
+async function generateAccessToken(userId: string) {
+    const payload = { userId }
+     return jwt.sign(payload, config.auth.secret, signOptions)
 }
 
 function peperify(password: string) {
@@ -15,5 +28,6 @@ function peperify(password: string) {
 
 
 export default {
+    generateAccessToken,
     hashPassword
 }
